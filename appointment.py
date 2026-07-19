e2d958b0604235d4b4896d35b2d30e7f79e6305e
hasvathvishvar2024 <hasvathvishva.r2024@vitstudent.ac.in>
2026-07-19 20:13:28 +0530

    Add login authentication feature function

--- appointment.py
+++ appointment.py
@@ -1,4 +1,7 @@
 print("Hospital Appointment System")
 def register_patient(name, age):
     print(f"Patient {name} ({age}) registered successfully!")
+def login_user(username):
+    print(f"User {username} logged in successfully!")
 register_patient("Lara",21)
+login_user("admin")
